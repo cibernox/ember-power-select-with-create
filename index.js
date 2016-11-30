51bb1209d2d90dcdd5692c9d7cd9c4bccd367660
cibernox <miguel.camba@gmail.com>
2016-11-30 09:17:38 +0000

    Allow usage without EPS

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-power-select-with-create'
+  name: 'ember-power-select-with-create',
+
+  contentFor: function(type, config) {
+    var emberPowerSelect = this.addons.filter(function(addon) {
+      return addon.name === 'ember-power-select';
+    })[0]
+    return emberPowerSelect.contentFor(type, config);
+  }
 };
