ee1ca4d6f945f0f29fcf0af966bc3caec17e4071
Zach Wentz <zkwentz@gmail.com>
2017-03-23 21:33:16 -0700

    made suggestedOption component tagless

--- addon/components/power-select-with-create/suggested-option.js
+++ addon/components/power-select-with-create/suggested-option.js
@@ -2,5 +2,6 @@ import Ember from 'ember';
 import layout from '../../templates/components/power-select-with-create/suggested-option';
 
 export default Ember.Component.extend({
+  tagName: '',
   layout
 });
