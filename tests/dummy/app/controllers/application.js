5b021950e6fa11012354add71f8b55463c242d67
Alex DiLiberto <alex@alexdiliberto.com>
2020-04-30 14:32:25 -0400

    linter fixes

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,60 +1,64 @@
-import { later } from '@ember/runloop';
-import { Promise } from 'rsvp';
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { Promise } from 'rsvp';
+import { later } from '@ember/runloop';
+import { A } from '@ember/array';
+import { action } from '@ember/object';
 
-const countries = [
-  { name: 'United States',  code: 'US', population: 321853000 },
-  { name: 'Spain',          code: 'ES', population: 46439864 },
-  { name: 'Portugal',       code: 'PT', population: 10374822 },
-  { name: 'Russia',         code: 'RU', population: 146588880 },
-  { name: 'Latvia',         code: 'LV', population: 1978300 },
-  { name: 'Brazil',         code: 'BR', population: 204921000 },
-  { name: 'United Kingdom', code: 'GB', population: 64596752 },
-];
+export default class ApplicationController extends Controller {
+  @tracked selectedCountry = null;
 
-export default Controller.extend({
-  countries,
-  slowPromise: null,
+  countries = A([
+    { name: 'United States',  code: 'US', population: 321853000 },
+    { name: 'Spain',          code: 'ES', population: 46439864 },
+    { name: 'Portugal',       code: 'PT', population: 10374822 },
+    { name: 'Russia',         code: 'RU', population: 146588880 },
+    { name: 'Latvia',         code: 'LV', population: 1978300 },
+    { name: 'Brazil',         code: 'BR', population: 204921000 },
+    { name: 'United Kingdom', code: 'GB', population: 64596752 },
+  ]);
+  selectedCountries = A();
+  slowPromise = null;
 
-  init() {
-    this._super(...arguments);
-    this.set('slowPromise', this.createSlowPromise());
-    this.set('selectedCountries', []);
-  },
+  constructor(owner, args) {
+    super(owner, args);
 
-  // Actions
-  actions: {
-    createCountry(countryName) {
-      let newCountry = { name: countryName, code: 'XX', population: 'unknown' };
-      this.get('countries').pushObject(newCountry);
-      this.set('selectedCountry', newCountry);
-      this.get('selectedCountries').push(newCountry);
-    },
+    this.slowPromise = this.createSlowPromise()
+  }
 
-    searchCountries(term) {
-      return new Promise((resolve, reject) => {
-        this.createSlowPromise(2000).then((countries) => {
-          resolve(countries.filter((country) => {
-            return country.name.toLowerCase().match(term.toLowerCase());
-          }));
-        }, reject);
-      });
-    },
+  @action
+  createCountry(countryName) {
+    let newCountry = { name: countryName, code: 'XX', population: 'unknown' };
 
-    hideCreateOptionOnSameName(term) {
-      let existingOption = this.get('countries').findBy('name', term);
-      return !existingOption;
-    },
-  },
+    this.countries.pushObject(newCountry);
+    this.selectedCountry = newCountry;
+    this.selectedCountries.pushObject(newCountry);
+  }
+  
+  @action
+  searchCountries(term) {
+    return new Promise((resolve, reject) => {
+      this.createSlowPromise(2000).then((countries) => {
+        resolve(countries.filter((country) => {
+          return country.name.toLowerCase().match(term.toLowerCase());
+        }));
+      }, reject);
+    });
+  }
 
-  // Methods
+  @action
+  hideCreateOptionOnSameName(term) {
+    let existingOption = this.countries.findBy('name', term);
+    return !existingOption;
+  }
+
   capitalizeSuggestion(term) {
     return `Hey! Perhaps you want to create ${term.toUpperCase()}??`;
-  },
+  }
 
   createSlowPromise(time = 5000) {
-    return new Promise(function(resolve) {
-      later(() => resolve(countries), time);
+    return new Promise((resolve) => {
+      later(() => resolve(this.countries), time);
     });
-  },
-});
+  }
+}
