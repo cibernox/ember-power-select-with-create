e1b8835dbe15314c0ef4892e6538547632b25098
Jonathan Newman <jonathan.newman@puppetlabs.com>
2022-08-03 05:40:06 -0700

    Update to Ember Power Select 5.0 (#130)
    This updates to the Ember Power Select version 5, and turns
    off a linter warning about the use of classic components which
    are used in the tests.
    
    Supports https://github.com/cibernox/ember-power-select-with-create/issues/129

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,8 @@ module.exports = {
     browser: true
   },
   rules: {
-    'ember/no-jquery': 'error'
+    'ember/no-jquery': 'error',
+    'ember/no-classic-components': 0,
   },
   overrides: [
     // node files
