bc26e2ad4a7b7e8b22a4c003289eef37dd2c1dac
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-06-06 07:58:06 +0200

    Update eps to v8

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 module.exports = {
   name: require('./package').name,
 
-  contentFor(type, config) {
-    var emberPowerSelect = this.addons.filter(function (addon) {
-      return addon.name === 'ember-power-select';
-    })[0];
-    return emberPowerSelect.contentFor(type, config);
-  },
+  // contentFor(type, config) {
+  //   var emberPowerSelect = this.addons.filter(function (addon) {
+  //     return addon.name === 'ember-power-select';
+  //   })[0];
+  //   return emberPowerSelect.contentFor(type, config);
+  // },
 };
--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -2,6 +2,7 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'dummy/config/environment';
+import 'ember-power-select/styles';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
